5979bdd0c55f03a2064dcb19dc1c983ea1254645
huggerkios <hugger.kiosk0r@icloud.com>
2024-09-18 09:04:13 +0300

    Update userToAdmin.js

--- src/api/userToAdmin.js
+++ src/api/userToAdmin.js
@@ -2,14 +2,13 @@ import Cookies from "js-cookie";
 import axios from "axios";
 
 async function userToAdmin(user) {
-  const host = String(import.meta.env.VITE_HOST);
-  const port = String(import.meta.env.VITE_PORT);
+  const base_url = import.meta.env.VITE_BASE_URL;
 
   const csrftoken = Cookies.get("csrftoken");
 
   try {
     const response = await axios.post(
-      `${host}:${port}/api/v1/users/${user.username}/admin-set/`,
+      `${base_url}/api/v1/users/${user.username}/admin-set/`,
       { is_admin: user.username },
       {
         headers: {
