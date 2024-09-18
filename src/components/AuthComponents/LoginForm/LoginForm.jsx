b5b03a354ae2a88f709f5dfde3d209a0c7bd7668
huggerkios <hugger.kiosk0r@icloud.com>
2024-09-18 09:09:10 +0300

    Update LoginForm.jsx

--- src/components/AuthComponents/LoginForm/LoginForm.jsx
+++ src/components/AuthComponents/LoginForm/LoginForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import loginUser from "../../../api/LoginUser";
+import loginUser from "../../../api/loginUser";
 
 function LoginForm() {
   const navigate = useNavigate();
