1efd82fca484b6b4b12d1a707abc9277734efc37
huggerkios <hugger.kiosk0r@icloud.com>
2024-09-18 09:04:39 +0300

    Update vierFile.js

--- src/api/vierFile.js
+++ src/api/vierFile.js
@@ -2,14 +2,13 @@ import Cookies from "js-cookie";
 import axios from "axios";
 
 async function vierFile(uuid) {
-  const host = String(import.meta.env.VITE_HOST);
-  const port = String(import.meta.env.VITE_PORT);
+  const base_url = import.meta.env.VITE_BASE_URL;
 
   const csrftoken = Cookies.get("csrftoken");
 
   try {
     const response = await axios.get(
-      `${host}:${port}/api/v1/cloud/files/${uuid}/`,
+      `${base_url}/api/v1/cloud/files/${uuid}/`,
       {
         headers: {
           "X-CSRFToken": csrftoken,
